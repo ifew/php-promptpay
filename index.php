b0478d2f8e8fb90307a84fd03eeee9ebc9910f70
Daud Salamiah <78581586+Daud613@users.noreply.github.com>
2021-02-15 06:53:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ use ThaiPromptpay\PromptpayQR;
 
 $qr = new PromptpayQR;
 $qr->setPromptpayType('02');
-$qr->setPromptpayID('4419928285542');
-$qr->setAmount(200.50);
+$qr->setPromptpayID('1969900166736');
+$qr->setAmount(100000.00);
 echo $qr->getQR();
 ?>
